bd9ee2d82faf7340cfc5ff73b6dbe344b830b57e
yogesh-hackx <yogesh.hackx@gmail.com>
2019-07-13 10:56:01 +0530

    Cleaned a bit of code(Asthetics :P)

--- whatsaBot.py
+++ whatsaBot.py
@@ -7,14 +7,17 @@ input("Scan QR press enter if scanned \n")
 
 print("Select a Contact from recents, or Enter 'c' to select from contact list: \n")
 
-recents = []
-sNo = 1
-for i in (driver.find_elements_by_xpath('//span[@class = "_19RFN"]')):
-	recents.append(i.text)
-	print(str(sNo)+" "+i.text)
-	sNo += 1
+#lists Cantacts from recents list:
+def printRecents():
+	recents = []
+	sNo = 1
+	for i in (driver.find_elements_by_xpath('//span[@class = "_19RFN"]')):
+		recents.append(i.text)
+		print(str(sNo)+"--> "+i.text)
+		sNo += 1
 
-option = input("\n") - 1
-
-user = (driver.find_elements_by_xpath('//span[@class = "_19RFN"]'))[option]
-user.click()
+#Selects a user based on selected option:
+def selectUser():
+	option = (int(input("\n")) - 1)
+	user = (driver.find_elements_by_xpath('//span[@class = "_19RFN"]'))[option]
+	user.click()
